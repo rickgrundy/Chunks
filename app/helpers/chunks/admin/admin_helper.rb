c90cfa902896707b9c8e178644bc5dfe8174b140
Rick Grundy <rick@rickgrundy.com>
2011-10-22 09:27:35 +0100

    Use page title helper

--- app/helpers/chunks/admin/admin_helper.rb
+++ app/helpers/chunks/admin/admin_helper.rb
@@ -1,7 +1,7 @@
 module Chunks::Admin::AdminHelper
   def title(text)
     content_for(:page_title, text)
-    content_for(:browser_title, "#{text} - #{Rails.application.class.parent_name} Chunks Admin")
+    content_for(:browser_title, "#{text} | #{Rails.application.class.parent_name} Chunks Admin")
   end
   
   def validation_errors(model)
