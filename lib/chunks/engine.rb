0d7861e1ef7b869751fdaba3223f0d7d3f684aca
Rick Grundy <rick@rickgrundy.com>
2011-10-22 09:30:16 +0100

    Don't need public static asset initializer since Rails 3.1 asset pipeline

--- lib/chunks/engine.rb
+++ lib/chunks/engine.rb
@@ -1,7 +1,4 @@
 module Chunks
   class Engine < Rails::Engine
-    initializer "chunks static assets" do |app|
-      app.middleware.insert_before(Rack::Lock, ActionDispatch::Static, "#{root}/public")
-    end    
   end
 end
