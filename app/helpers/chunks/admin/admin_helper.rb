855ff5370faba326cd269900aec4e194cd6ab8d1
Rick Grundy <rick@rickgrundy.com>
2011-10-29 12:20:36 +0100

    Cleaner handling of validation error messages

--- app/helpers/chunks/admin/admin_helper.rb
+++ app/helpers/chunks/admin/admin_helper.rb
@@ -1,5 +1,5 @@
 module Chunks::Admin::AdminHelper
-  def title(text)
+  def page_title(text)
     content_for(:page_title, text)
     content_for(:browser_title, "#{text} - #{Rails.application.class.parent_name} Chunks Admin")
   end
@@ -8,11 +8,16 @@ module Chunks::Admin::AdminHelper
     model.new_record? ? send("chunks_admin_#{model.class.table_name}_path") : send("chunks_admin_#{model.class.table_name.singularize}_path", model)
   end
   
-  def validation_errors(model)
-    raw <<-ERRORS
-      <ul class="errors">
-        #{model.errors.full_messages.map { |e| "<li>#{e}</li>" }.join}
-      </ul>
-    ERRORS
+  def validation_errors(model, title=nil)
+    return if model.errors.empty?
+    without_associated_fields = model.errors.reject { |error| error.first.to_s.include?(".") }
+    error_messages = without_associated_fields.map do |error| 
+      field, msg = *error
+      "#{field.to_s.humanize} #{msg}"
+    end
+    render partial: "chunks/admin/common/errors", locals: {
+      title: title || "#{model.class.model_name.demodulize.humanize} could not be saved", 
+      error_messages: error_messages
+    }
   end
 end
--- spec/helpers/chunks/admin/admin_helper_spec.rb
+++ spec/helpers/chunks/admin/admin_helper_spec.rb
@@ -12,4 +12,37 @@ describe Chunks::Admin::AdminHelper do
       helper.chunks_admin_resource_path(page).should == "/chunks/admin/pages/#{page.id}"
     end
   end
+  
+  describe "rendering validation errors" do
+    before(:each) do
+      @untitled_page = Factory.build(:page)
+      @untitled_page.title = nil
+    end
+    
+    it "does not render anything if model is valid" do
+      helper.validation_errors(Factory(:page)).should be_nil
+    end
+    
+    it "does not render anything if model has not been validated" do
+      helper.validation_errors(@untitled_page).should be_nil
+    end
+    
+    it "renders the errors partial if model is invalid" do
+      @untitled_page.valid?
+      content = helper.validation_errors(@untitled_page)
+      content.should include "Title can't be blank"
+    end
+    
+    it "accepts a custom title" do
+      @untitled_page.valid?
+      content = helper.validation_errors(@untitled_page, "Disaster!")
+      content.should include "Disaster!"
+    end
+    
+    it "uses a default title if not specified" do
+      @untitled_page.valid?
+      content = helper.validation_errors(@untitled_page)
+      content.should include "Page could not be saved"
+    end
+  end
 end
