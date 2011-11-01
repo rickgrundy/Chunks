1596d811655a674b898e5d09afe40a6e754483b4
Rick Grundy <rick@rickgrundy.com>
2011-11-01 08:31:20 +0000

    Extracted postToIframeDialog to a jQuery extension.

--- app/assets/javascripts/chunks/admin/lib/util.js
+++ app/assets/javascripts/chunks/admin/lib/util.js
@@ -4,3 +4,40 @@ function guid() {
   }
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
+
+$.extend($.fn, { 
+  cloneFormFields: function() {
+    var clone = $("<div/>").append(this.find(":input").clone());
+    this.find("textarea").each(function() {
+      var orig = $(this);
+      clone.find("textarea[name='" + orig.attr("name") + "']").val(orig.val());
+    });
+    return clone;
+  },
+  
+  postToIframeDialog: function(opts) {
+    var iframeGuid = guid();
+    var container = $("<div style='position: relative;'/>");
+    var iframe = $("<iframe id='" + iframeGuid + "' name='" + iframeGuid + "'/>").appendTo(container);
+    var eventShield = $("<div style='position: absolute; top: 0; left: 0;'/>");
+
+    function resizeIframeToMatchContainer() {
+      var height = container.dialog("widget").find(".ui-dialog-content").height();
+      iframe.css({height: height+"px"});
+      eventShield.css({height: height+"px", width: "100%"});
+    }
+
+    container.dialog({
+       title: opts.title,
+       width: opts.width,
+       resizeStart: function() { eventShield.appendTo(container) },
+       resizeStop: function() { eventShield.remove() },
+       resize: resizeIframeToMatchContainer,
+       open: resizeIframeToMatchContainer
+    });
+
+    form = $("<form action='" + opts.url + "' method='post' target='" + iframeGuid + "'/>");
+    form.append("<input type='hidden' name='iframe_id' value='" + iframeGuid + "'/>");
+    form.append(this.cloneFormFields()).submit();
+  }
+})
--- app/assets/javascripts/chunks/admin/page_edit.js.coffee
+++ app/assets/javascripts/chunks/admin/page_edit.js.coffee
@@ -13,33 +13,9 @@ initShowHelp = (chunk, button) ->
       width: 600
       
 initShowPreview = (chunk, button) ->
-  button.click ->
-    renderFormInIframeDialog("#{chunk.data("title")} Preview", button.attr("href"), chunk)
+  button.click ->    
+    chunk.postToIframeDialog
+      url: button.attr("href")
+      title: "#{chunk.data("title")} Preview"
+      width: 600
     false
-    
-    
-    
-renderFormInIframeDialog = (dialogTitle, href, fields) ->
-  iframe_guid = guid()
-  preview = $("<div style='position: relative;'/>")
-  iframe = $("<iframe id='#{iframe_guid}' name='#{iframe_guid}'/>").appendTo(preview)
-  eventCatcher = $("<div style='position: absolute; top: 0; left: 0;'/>")
-  resizeIframe = ->
-    height = preview.dialog("widget").find(".ui-dialog-content").height()
-    iframe.css
-      height: height+"px"
-    eventCatcher.css
-      height: height+"px"
-      width: "100%"
-  preview.dialog
-    title: dialogTitle
-    width: 600
-    resizeStart: -> preview.append(eventCatcher)
-    resizeStop: -> eventCatcher.remove()
-    resize: resizeIframe
-    open: resizeIframe
-  form = $("<form action='" + href + "' method='post' target='#{iframe_guid}'/>")
-  form.append("<input type='hidden' name='iframe_id' value='#{iframe_guid}'/>")
-  cloned = fields.clone()
-  cloned.find("textarea[name='#{$(textarea).attr('name')}']").val($(textarea).val()) for textarea in fields.find("textarea")  
-  form.append(cloned).submit()
